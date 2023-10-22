9fe094fa8e3cf1f6c1df1f3a0b70eaa93ac1b750
Shahidul Islam <webdesignershahid@gmail.com>
2023-10-22 16:33:45 +0600

    responsive fixed on 62inch device

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,86 @@
 (function ($) {
     "use strict";
 
-        var myCustomSlider = document.querySelectorAll('.gallerySlider');
-        var myCustomGalleryThumbs = document.querySelectorAll('.galleryThumbslider');
-
-        for (var i = 0; i < myCustomSlider.length; i++) {
-
-            myCustomSlider[i].classList.add('gallerySlider-' + i);
-            myCustomGalleryThumbs[i].classList.add('thumbs-class-' + i);
-
-            var galleryThumbs = new Swiper('.thumbs-class-' + i , {
-                slidesPerView: 2,
-                spaceBetween: 15,
-                autoplay: false,
-                speed: 1000,
-                breakpoints: {
-                    768: {
-                        slidesPerView: 4,
-                    },
-                    1200: {
-                        slidesPerView: 6,
-                        spaceBetween: 15,
-                    },
-                },
-                navigation: {
-                    nextEl: '.swiper-button-prev',
-                    prevEl: '.swiper-button-next',
-                },
-            });
-
-            var galleryTop = new Swiper('.gallerySlider-' + i, {
-                spaceBetween: 10,
-                thumbs: {
-                    // el: '.thumbs-class',
-                    // slidesPerView: 5,
-                    swiper: galleryThumbs
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            }); 
-
+    // Preloader
+    $(window).on('DOMContentLoaded', function() {
+        if ($('.preloader').length) {
+            $('.preloader').delay(2500).fadeOut(500);
         }
+    });
+
+    // Modal Gallery Slider with thumbnail
+    var myCustomSlider = document.querySelectorAll('.gallerySlider');
+    var myCustomGalleryThumbs = document.querySelectorAll('.galleryThumbslider');
+
+    for (var i = 0; i < myCustomSlider.length; i++) {
+
+        myCustomSlider[i].classList.add('gallerySlider-' + i);
+        myCustomGalleryThumbs[i].classList.add('thumbs-class-' + i);
+
+        var galleryThumbs = new Swiper('.thumbs-class-' + i , {
+            slidesPerView: 2,
+            spaceBetween: 15,
+            autoplay: false,
+            speed: 1000,
+            breakpoints: {
+                768: {
+                    slidesPerView: 4,
+                },
+                1200: {
+                    slidesPerView: 6,
+                    spaceBetween: 15,
+                },
+            },
+            navigation: {
+                nextEl: '.swiper-button-prev',
+                prevEl: '.swiper-button-next',
+            },
+        });
+
+        var galleryTop = new Swiper('.gallerySlider-' + i, {
+            spaceBetween: 10,
+            thumbs: {
+                swiper: galleryThumbs
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        }); 
+
+    }
+
+
+    // Select2 list formate with line break for en and ar
+    function formatDesign(item) {
+        var selectionText = item.text.split("*");
+        var $item = $(
+            '<div>' + selectionText[0] + '</div>' + '<div>' + selectionText[1] + '</div>'
+        );
+        return $item;
+    }
+
+    // Initialize select2
+    $('.select2').select2({
+        minimumResultsForSearch: Infinity, // disable search box
+        templateResult: formatDesign,
+        templateSelection: formatDesign,        
+    });
+
+    // show modal of the value from select option
+    $('.select2').change(function() {
+        var selectedOption = $(this).val();
+        $('#modal_' + selectedOption).modal('show');
+    });
+
+    // Location mapping image responsive
+    $(document).ready(function(e) {
+        $('img[usemap]').rwdImageMaps();        
+    });
+
+    // Wow js Animation
+    new WOW({
+        mobile:false
+    }).init();
 
 })(jQuery);
-    
-
-/* ============================================================ */
-/* PRELOADER
-/* ============================================================ */
-$(window).on('load', function() {
-    $(".preloader").fadeOut();     
-});
